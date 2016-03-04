e061e43a1c9c17af633c33bb04500f7465618cf2
georgesk <georgesk@debian.org>
2016-03-04 20:05:39 +0100

    implemented md5 hashing

--- js/gettext.js
+++ js/gettext.js
@@ -1,17 +1,14 @@
-var translatable; // jQuery array of translatable elements
-
 $(function(){ // to be called at startup
-    translatable=$(".tr");
+    // gathers translatable elements
+    window.translatable=$(".tr");
 });
 
 /**
  * Computes a hash for translatable inner HTML
  * @param el a jQuery element
- @ @return a MD5 hash
+ @ @return a string with a MD5 hash
  **/
 function trHash(el){
     var inner = el.html().replace(/\s/,"","m");
-    console.log(el, inner);
-    var hash=CryptoJS.MD5(inner);
-    console.log(hash);
+    return hash=CryptoJS.MD5(inner).toString();
 }
