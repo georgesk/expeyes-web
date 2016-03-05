80e0eab45afc1fa0c204a0b8c3ccc0676f245b83
georgesk <georgesk@debian.org>
2016-03-05 23:05:50 +0100

    extracted a hash method

--- translator/hashtable.py
+++ translator/hashtable.py
@@ -8,6 +8,17 @@ language supported by this po file
 import sys, re
 from hashlib import md5
 
+spaces=re.compile(r"\s")
+
+def toHash(s):
+    """
+    Creates a hash from a string, after removing every space char
+    @param s input string
+    @return a hash string
+    """
+    mini=spaces.sub("",s).encode("utf-8")
+    return md5(mini).hexdigest()
+
 idPattern=re.compile(r'^msgid (".*")$')
 strPattern=re.compile(r'^msgstr (".*")$')
 usualPattern=re.compile(r'^(".*")$')
@@ -53,10 +64,8 @@ class HashTable:
         @return a dictionary of MD5 hashes replacing the msgIds
         """
         result={}
-        spaces=re.compile(r"\s")
         for k in self.data:
-            kmini=spaces.sub("",k).encode("utf-8")
-            result[md5(kmini).hexdigest()]=self.data[k]
+            result[toHash(k)]=self.data[k]
         return result
     
 if __name__=="__main__":
