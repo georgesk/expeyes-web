223c21e24dd23202a03ee56cdad1d99c7c9fe379
georgesk <georgesk@debian.org>
2016-03-05 01:28:36 +0100

    the server is in a mixed state, probably because of cross-browsing

--- js/gettext.js
+++ js/gettext.js
@@ -1,6 +1,18 @@
 $(function(){ // to be called at startup
     // gathers translatable elements
     window.translatable=$(".tr");
+    for (var i=0; i<window.translatable.length; i++){
+	var tr=$(window.translatable[i]);
+	console.log(trHash(tr));
+	$.get(
+	    "http://georges.khaznadar.fr:8081/translate",
+	    {
+		lang:"fr",
+		key: trHash(tr),
+	    }).done(function(data){
+		console.log("got data:", data)
+	    });
+    }
 });
 
 /**
