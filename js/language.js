b56c845075b23b100ce4cdb72f464399316ad11b
georgesk <georgesk@debian.org>
2016-03-16 16:43:16 +0100

    first working version of language.js

--- js/language.js
+++ js/language.js
@@ -1,15 +1,32 @@
 $(function(){ 
     var supportedLang=["en", "fr"];
+    var path=window.location.pathname;
+    if (! path || path=="/"){
+	path="/index.html";
+    }
+    for(var l =0; l < supportedLang.length; l++){
+	// computes the path of the English page
+	var lang=supportedLang[l];
+	var prefix="/"+supportedLang[l]+"/"
+	if (path.startsWith(prefix)){
+	    path=path.substr(prefix.length - 1);
+	    break;
+	}
+    }
     var mynb=$("#mynb");
     var langFlags=$("<div>",{id: "languages"});
     for(var l =0; l < supportedLang.length; l++){
-	var target="/";
-	if (l!=0){ //non-English languages
-	    target="/"+supportedLang[l]+"/";
+	var lang=supportedLang[l];
+	var prefix="/"+supportedLang[l]+"/"
+	var target="";
+	if (l==0){ // English
+	    target=path;
+	} else { //non-English languages
+	    target="/"+lang+ path;
 	}
 	var anchor=$("<a>",{href: target, "class": "lang"});
-	var img=$("<img>",{src: "images/lang-"+supportedLang[l]+".png", alt: supportedLang[l]+ " language"});
-	anchor.text(supportedLang[l]);
+	var img=$("<img>",{src: "images/lang-"+lang+".png", alt: lang+" language"});
+	anchor.text(lang);
 	anchor.append(img)
 	mynb.append(anchor);
     }
